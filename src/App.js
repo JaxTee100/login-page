79446990241421e9ddc2aadf8e765fbe9c538571
Tobias <redefineddevelopertee@gmail.com>
2023-03-20 12:06:37 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import MainRegister from './components/MainRegister';
+
+function App() {
+  return (
+    <div className='container'>
+      <Header />
+      <MainRegister />
+      <Footer />
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Header = () =>{
+    return(
+        <header>
+            <h1>RiVi</h1>
+            <p><i>by <strong>ResMed</strong></i></p>
+        </header>
+    )
+}
+
+export default Header
